b56be88be945a7149c159e7c4a59eda3e24cf5ef
lam.khuong <lamkn06@gmail.com>
2022-10-30 08:19:28 +0700

    style: new container layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,30 @@
-import { Text, VStack } from "@chakra-ui/react";
+import { Grid, GridItem, Text, VStack, Box, Container } from "@chakra-ui/react";
 import { Add } from "../components/Add";
 
 const MainPage = () => {
   return (
-    <VStack p={5}>
-      <Text
-        bgGradient="linear(to-l, #2C7A7B,#9ccccd)"
-        bgClip="text"
-        fontSize="6xl"
-        fontWeight="extrabold"
-      >
-        Todo App
-      </Text>
+    <>
+      <VStack p={10}>
+        <Box>
+          <Text
+            bgGradient="linear(to-t,#224f51,  #8ad3d6)"
+            bgClip="text"
+            fontSize="6xl"
+            fontWeight="extrabold"
+          >
+            Todo App
+          </Text>
 
-      <Add />
-    </VStack>
+          <Add />
+        </Box>
+      </VStack>
+      <Container maxW={"1024px"}>
+        <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+          <GridItem w="100%" h="10" bg="blue.500" maxW={"400px"} />
+          <GridItem w="100%" h="10" bg="blue.500" maxW={"400px"} />
+        </Grid>
+      </Container>
+    </>
   );
 };
 
