18f9ac85d6aeda89913e531ace656e8383936cfa
lam.khuong <lamkn06@gmail.com>
2023-08-27 09:53:51 +0700

    chore: test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
-import { Box, Container, Text, VStack } from "@chakra-ui/react";
-import { Add } from "../components/Add";
-import { Content } from "../components/Contents";
+import {
+  Box,
+  Container,
+  Img,
+  List,
+  ListIcon,
+  ListItem,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { RepeatClockIcon } from "@chakra-ui/icons";
+import Countdown from "react-countdown";
 
 const MainPage = () => {
   return (
@@ -8,18 +17,108 @@ const MainPage = () => {
       <VStack p={10}>
         <Box>
           <Text
-            bgGradient="linear(to-r,#7928ca,  #ff0080)"
+            bgGradient="linear(to-r,#00497a,  #00497a)"
             bgClip="text"
             fontSize="6xl"
             fontWeight="extrabold"
           >
-            Todos Lam Intern
+            <Img src="https://www.trueprofile.io/_next/static/images/logo-blue-fd70ec0ee0aec283e82090abf80048ca.svg" />
           </Text>
-          <Add />
         </Box>
       </VStack>
-      <Container maxW={"1024px"} display="flex" justifyContent={"center"}>
-        <Content />
+      <Container
+        maxW={"1024px"}
+        display="flex"
+        justifyContent={"center"}
+        flexDirection={"column"}
+        alignItems={"center"}
+      >
+        <Countdown
+          date={new Date("2023-11-30")}
+          renderer={(props) => {
+            const { days, hours, minutes, seconds } = props;
+            return (
+              <>
+                <ul id="countdown">
+                  <li id="days">
+                    <div className="number">
+                      <Text
+                        bgGradient="linear(to-r,#00497a,  #00497a)"
+                        bgClip="text"
+                      >
+                        {days}
+                      </Text>
+                    </div>
+                    <div className="label">Days</div>
+                  </li>
+                  <li id="hours">
+                    <div className="number">
+                      <Text
+                        bgGradient="linear(to-r,#00497a,  #00497a)"
+                        bgClip="text"
+                      >
+                        {hours}
+                      </Text>
+                    </div>
+                    <div className="label">Hours</div>
+                  </li>
+                  <li id="minutes">
+                    <div className="number">
+                      <Text
+                        bgGradient="linear(to-r,#00497a,  #00497a)"
+                        bgClip="text"
+                      >
+                        {minutes}
+                      </Text>
+                    </div>
+                    <div className="label">Minutes</div>
+                  </li>
+                  <li id="seconds">
+                    <div className="number">
+                      <Text
+                        bgGradient="linear(to-r,#00497a,  #00497a)"
+                        bgClip="text"
+                      >
+                        {seconds}
+                      </Text>
+                    </div>
+                    <div className="label">Seconds</div>
+                  </li>
+                </ul>
+              </>
+            );
+          }}
+        />
+        <List spacing={3} marginTop={"50px"}>
+          <ListItem>
+            <ListIcon as={RepeatClockIcon} color="red.500" />
+            Lam Intern {"==>"} on Queue
+          </ListItem>
+          <ListItem>
+            <ListIcon as={RepeatClockIcon} color="red.500" />
+            Thang Bui {"==>"} on Queue
+          </ListItem>
+          <ListItem>
+            <ListIcon as={RepeatClockIcon} color="red.500" />
+            Cuong Huynh {"==>"} on Queue
+          </ListItem>
+          <ListItem>
+            <ListIcon as={RepeatClockIcon} color="red.500" />
+            Quan Van {"==>"} on Queue
+          </ListItem>
+          <ListItem>
+            <ListIcon as={RepeatClockIcon} color="red.500" />
+            Quan Pham {"==>"} on Queue
+          </ListItem>
+          <ListItem>
+            <ListIcon as={RepeatClockIcon} color="red.500" />
+            Kim Do {"==>"} on Queue
+          </ListItem>
+          <ListItem>
+            <ListIcon as={RepeatClockIcon} color="red.500" />
+            Bao Vu {"==>"} on Queue
+          </ListItem>
+        </List>
       </Container>
     </>
   );
